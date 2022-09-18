9c35d4bd6c4772cbed536cd8e93a49b59035e805
Sijina Padanattu Aliyar <63553337+sijinaaliyar@users.noreply.github.com>
2022-09-18 14:09:48 +0100

    Update train_triplet.py

--- train_triplet.py
+++ train_triplet.py
@@ -6,7 +6,7 @@ from keras.layers.pooling import MaxPooling2D, AveragePooling2D
 from keras.layers.merge import Concatenate
 from keras.layers.core import Lambda, Flatten, Dense
 from keras.initializers import glorot_uniform
-from keras.engine.topology import Layer
+from tensorflow.keras.layers import Layer, InputSpec
 from keras import backend as K
 K.set_image_data_format('channels_first')
 import cv2
