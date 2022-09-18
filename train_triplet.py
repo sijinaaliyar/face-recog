f7e6a1896d234b8ba49c0412056718c2a79df176
Sijina Padanattu Aliyar <63553337+sijinaaliyar@users.noreply.github.com>
2022-09-18 14:08:22 +0100

    Update train_triplet.py

--- train_triplet.py
+++ train_triplet.py
@@ -1,7 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
 from keras.models import Model
-from keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 from keras.layers.pooling import MaxPooling2D, AveragePooling2D
 from keras.layers.merge import Concatenate
 from keras.layers.core import Lambda, Flatten, Dense
